7623e49b41c266a12e7b941c8f6c15edfe62c377
saeedeMasoudinejad <58415361+saeedeMasoudinejad@users.noreply.github.com>
2024-05-07 23:03:51 +0330

    Add swagger urls

--- core/urls.py
+++ core/urls.py
@@ -17,8 +17,25 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+# Swagger Config
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Wallet System",
+        default_version='v1',
+        description="Tosan Assignment, A simple wallet system that allows users to create wallets with limitations "
+                    "and perform deposits, withdrawals, and refunds.",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
 ]
