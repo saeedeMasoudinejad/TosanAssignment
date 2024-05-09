f3b570c7ebc709f0ec544395625ea2cf3ff1cfa0
saeedeMasoudinejad <58415361+saeedeMasoudinejad@users.noreply.github.com>
2024-05-10 00:26:57 +0330

    Define url for profile.

--- user/urls.py
+++ user/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from user.views import UserSignupView
+from user.views import UserSignupView, ProfileViewSet
 
 urlpatterns = [
     path('signup/', UserSignupView.as_view(), name='signup'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('refresh_token/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', ProfileViewSet.as_view({'get': 'retrieve'}), name='profile')
 ]
