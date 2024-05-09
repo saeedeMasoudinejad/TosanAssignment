d78973096009edf1849739042183a554be1cd15a
saeedeMasoudinejad <58415361+saeedeMasoudinejad@users.noreply.github.com>
2024-05-10 00:24:32 +0330

    Define readonly serializer for show the user profile with wallet info.

--- user/serializers.py
+++ user/serializers.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from finance.serializers import WalletModelSerializer
+
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     def create(self, validated_data):
-        print(validated_data)
         user = User.objects.create_user(
             username=validated_data['username'],
             password=validated_data['password']
@@ -18,4 +19,18 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'password']
         read_only_fields = (
             'id',
+        )
+
+
+class ProfileReadOnlySerializer(serializers.ModelSerializer):
+    wallets = WalletModelSerializer(
+        many=True,
+        read_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'wallets'
         )
