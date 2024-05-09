335979973eba7cc94abf81520b0a0d6718bb5906
saeedeMasoudinejad <58415361+saeedeMasoudinejad@users.noreply.github.com>
2024-05-10 00:23:26 +0330

    Modify the TransactionWriteOnlySerializer(change the model serializer to serializer)
    Define the serializer for show the transaction history.

--- finance/serializers.py
+++ finance/serializers.py
@@ -1,6 +1,7 @@
+from django.core.validators import MinValueValidator
 from rest_framework import serializers
 
-from finance.models import Wallet
+from finance.models import Wallet, Transaction
 
 
 class WalletModelSerializer(serializers.ModelSerializer):
@@ -15,3 +16,34 @@ class WalletModelSerializer(serializers.ModelSerializer):
             'balance',
             'wallet_number',
         )
+
+
+class TransactionWriteOnlySerializer(serializers.Serializer):
+    sender_wallet_number = serializers.CharField()
+    receiver_wallet_number = serializers.CharField()
+    amount = serializers.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(1000)]
+    )
+
+
+class TransactionReadOnlyModelSerializer(serializers.ModelSerializer):
+    sender_wallet = WalletModelSerializer(read_only=True)
+    receiver_wallet = WalletModelSerializer(read_only=True)
+    # transaction_type = serializers.SerializerMethodField()
+    class Meta:
+        model = Transaction
+        fields = (
+            'updated_at',
+            'sender_wallet',
+            'receiver_wallet',
+            'amount',
+            # 'transaction_type',
+        )
+
+    # def get_transaction_type(self,
+    #                          obj)
+    #     if obj.sender_wallet == self.context['request'].user:
+    #         transaction_type = 'withdrew'
+    #     else
