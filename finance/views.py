a8fd5346c53c0b0e6964672c578aab52029840ef
saeedeMasoudinejad <58415361+saeedeMasoudinejad@users.noreply.github.com>
2024-05-11 14:03:10 +0330

    Define history transaction view. Define refund function. PEP8

--- finance/views.py
+++ finance/views.py
@@ -1,20 +1,33 @@
-import os
-from decimal import Decimal, ROUND_HALF_UP
-
 from django.db.models import F, Q
 from rest_framework import mixins, status
 from rest_framework.decorators import action
 from rest_framework.exceptions import NotFound
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from rest_framework.viewsets import GenericViewSet
 
-from core.choice_type_fields import TransactionStatusChoice, TransactionTypeChoice
+from core.choice_type_fields import (
+    TransactionStatusChoice,
+    TransactionTypeChoice
+)
 from core.message_text import MessageText
+from core.paginations import CustomPaginator
 from core.utils import convertor
-from finance.models import Wallet, Transaction
-from finance.serializers import TransactionWriteOnlySerializer, \
-    TransactionReadOnlyModelSerializer, WalletModelSerializer
-from django.db import transaction, IntegrityError, OperationalError
+from finance.models import (
+    Wallet,
+    Transaction
+)
+from finance.permissions import IsWalletOwner
+from finance.serializers import (
+    TransactionWriteOnlySerializer,
+    TransactionReadOnlyModelSerializer,
+    WalletModelSerializer
+)
+from django.db import (
+    transaction,
+    IntegrityError,
+    OperationalError
+)
 
 
 class WalletCreation(
@@ -27,99 +40,18 @@ class WalletCreation(
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
 
-    # def create(self, request, *args, **kwargs):
-    #     request.data['owner'] = self.request.user.id
-    #     serializer = self.get_serializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     return super(WalletCreation, self).create(request, *args, **kwargs)
-
-    # @action(
-    #     detail=False,
-    #     methods=['GET'],
-    # )
-    # def wallet_creation(self, request, *args, **kwargs):
-    #     # This api does not require any input parameters for creating a wallet,
-    #     # and there are no plans for further development.
-    #     # The Single Responsibility Principle is implemented with the GET request.
-    #     wallet = Wallet.objects.create(owner=request.user)
-    #     serializer = self.get_serializer(wallet)
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
-
 
 class TransactionViewSet(
-    mixins.CreateModelMixin,
-    mixins.ListModelMixin,
     GenericViewSet
 ):
     serializer_class = TransactionWriteOnlySerializer
     queryset = Transaction.objects.all()
 
-    def get_queryset(self):
-        if self.action == 'list':
-            return Transaction.objects.filter(
-                Q(origin__owner=self.request.user) |
-                Q(destination__owner=self.request.user)
-            )
-        return self.queryset
-
-    def get_serializer_class(self):
-        if self.action == 'list':
-            return TransactionReadOnlyModelSerializer
-        return self.serializer_class
-
-    # @action(detail=False, methods=['POST'])
-    # def deposit(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     data = serializer.validated_data
-    #     amount = data['amount']
-    #     sender_wallet_number = data['sender_wallet_number']['wallet_number']
-    #     receiver_wallet_number = data['receiver_wallet_number']['wallet_number']
-    #     try:
-    #         sender_wallet = Wallet.objects.get(
-    #             owner=self.request.user.id,
-    #             _wallet_number=sender_wallet_number
-    #         )
-    #     except Wallet.DoesNotExist:
-    #         raise NotFound(MessageText.WalletNumberIsNotBelong)
-    #     try:
-    #         receiver_wallet = Wallet.objects.get(
-    #             _wallet_number=receiver_wallet_number
-    #         )
-    #     except Wallet.DoesNotExist:
-    #         raise NotFound(MessageText.WalletNumberNotFound)
-    #     with transaction.atomic():
-    #         finance_transaction = Transaction.objects.create(
-    #             sender_wallet=sender_wallet,
-    #             receiver_wallet=receiver_wallet,
-    #             amount=amount
-    #         )
-    #         if amount > sender_wallet.balance or Transaction.is_daily_transfer_limit_exceeded(sender_wallet,
-    #                                                                                           amount):
-    #             # TODO: call the refund method
-    #             finance_transaction.status = TransactionStatusChoice.FAILED
-    #             finance_transaction.save()
-    #             return Response(
-    #                 exception=MessageText.DailyTransferLimitExceeded,
-    #                 status=status.HTTP_400_BAD_REQUEST
-    #             )
-    #         else:
-    #             sender_wallet.balance -= amount
-    #             receiver_wallet.balance += amount
-    #             sender_wallet.save()
-    #             receiver_wallet.save()
-    #             finance_transaction.status = TransactionStatusChoice.COMPLETE
-    #             finance_transaction.save()
-    #             return Response(
-    #                 serializer.data,
-    #                 status=status.HTTP_200_OK
-    #             )
-
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         data = serializer.validated_data
-        amount = convertor(data['amount'],'int')
+        amount = convertor(data['amount'], 'int')
         origin_wallet_number = data['origin_wallet_number']
         destination_wallet_number = data['destination_wallet_number']
 
@@ -140,7 +72,8 @@ class TransactionViewSet(
         except Wallet.DoesNotExist:
             raise NotFound(MessageText.WalletNumberNotFound)
 
-        if amount > origin_wallet._balance or Transaction.is_daily_transfer_limit_exceeded(origin_wallet, amount):
+        if amount > origin_wallet._balance or \
+                Transaction.is_daily_transfer_limit_exceeded(origin_wallet, amount):
             status_choice = TransactionStatusChoice.FAILED
             exception_message = MessageText.DailyTransferLimitExceeded
         else:
@@ -180,3 +113,65 @@ class TransactionViewSet(
                 serializer.data,
                 status=status.HTTP_200_OK
             )
+
+    @action(
+        detail=True,
+        methods=['get']
+    )
+    def refund(self, request, pk='transaction_number'):
+        origin_transaction = get_object_or_404(Transaction, transaction_number=pk)
+        origin_wallet = origin_transaction.origin
+        destination_wallet = origin_transaction.destination
+        amount = convertor(origin_transaction.amount, 'int')
+        if origin_transaction.status == TransactionStatusChoice.COMPLETE and \
+                not hasattr(origin_transaction, 'refund_transactions'):
+            with transaction.atomic():
+                try:
+                    refund_transaction = Transaction.objects.create(
+                        origin=origin_wallet,
+                        destination=destination_wallet,
+                        _amount=amount,
+                        status=TransactionStatusChoice.PENDING,
+                        transaction_type=TransactionTypeChoice.REFUNDED,
+                        original_transaction=origin_transaction
+                    )
+                except (IntegrityError, OperationalError):
+                    # Transaction creation failed due to concurrent transaction
+                    raise IntegrityError("Concurrent transaction, please try again.")
+
+                origin_wallet._balance = F('_balance') + amount
+                destination_wallet._balance = F('_balance') - amount
+                origin_wallet.save()
+                destination_wallet.save()
+                refund_transaction.status = TransactionStatusChoice.COMPLETE
+                refund_transaction.save()
+                refund_transaction.refresh_from_db()
+                return Response(
+                    TransactionReadOnlyModelSerializer(refund_transaction).data,
+                    status=status.HTTP_200_OK
+                )
+        else:
+            return Response(
+                MessageText.TransactionIsNotRefunded,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+class TransactionHistoryListAPIView(
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    permission_classes = [IsWalletOwner]
+    serializer_class = TransactionReadOnlyModelSerializer
+    pagination_class = CustomPaginator
+
+    def get_serializer_context(self, *args, **kwargs):
+        context = super().get_serializer_context()
+        context['wallet_number'] = self.kwargs['wallet_number']
+        return context
+
+    def get_queryset(self):
+        queryset = Transaction.objects.filter(Q(origin___wallet_number=self.kwargs.get('wallet_number')) |
+                                              Q(destination___wallet_number=self.kwargs.get('wallet_number'))
+                                              )
+        return queryset
